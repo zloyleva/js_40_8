e4e42b0e6ffc7220232cb9122b2f5ddce7fd0bd7
ZloyLeva <zloyleva@gmail.com>
2019-02-02 12:37:46 +0200

    add Lib H5

--- src/js/lib/FactoryHtml.js
+++ src/js/lib/FactoryHtml.js
@@ -7,6 +7,7 @@ import Nav from "./Nav";
 import Li from "./Li";
 import Ul from "./Ul";
 import Span from "./Span";
+import H5 from "./H5";
 
 export default function (tag) {
     switch (tag) {
@@ -16,6 +17,8 @@ export default function (tag) {
             return new Button();
         case "img":
             return new Img();
+        case "h5":
+            return new H5();
         case "nav":
             return new Nav();
         case "li":
---
+++ src/js/lib/H5.js
@@ -0,0 +1,7 @@
+import BuilderHtml from "./BuilderHtml";
+
+
+export default function H5() {
+    const tag = "h5";
+    BuilderHtml.call(this, tag);
+}
