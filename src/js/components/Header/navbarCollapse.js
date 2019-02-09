c88f0d41c33922084ef1ce2611d2ca024931cdbd
ZloyLeva <zloyleva@gmail.com>
2019-02-09 10:58:46 +0200

    refactoring Nav menu

--- src/js/components/Header/navbarCollapse.js
+++ src/js/components/Header/navbarCollapse.js
@@ -5,33 +5,30 @@ const a = getHtmlObject("a");
 const ul = getHtmlObject("ul");
 const li = getHtmlObject("li");
 
-const navItems = [];
-navItems.push(li.createHtmlElement(
-    "nav-item",
-    null,
-    [
-        a.createHtmlElement(
-            "nav-link",
-            [{name:"href", value:"/"}],
-            "Home"
-        )
-    ]
-));
-navItems.push(li.createHtmlElement(
-    "nav-item",
-    null,
-    [
-        a.createHtmlElement(
-            "nav-link",
-            [{name:"href", value:"#"}],
-            "Catalog"
-        )
-    ]
-));
+const navLinks = [
+    {name: "Home", href: "/"},
+    {name: "Catalog", href: "#"},
+    {name: "Cart", href: "#"},
+    {name: "Contact", href: "#"},
+];
+
+const navItems = navLinks.map((el) =>
+    li.createHtmlElement(
+        "nav-item",
+        null,
+        [
+            a.createHtmlElement(
+                "nav-link",
+                [{name:"href", value:el.href}],
+                el.name
+            )
+        ]
+    )
+);
 
 const navbarNav = ul.createHtmlElement("navbar-nav",null,navItems);
 const navbarCollapse = div.createHtmlElement(
-    "collapse navbar-collapse",
+    "collapse navbar-collapse justify-content-end",
     [{name:"id", value:"navbarSupportedContent"}],
     [navbarNav]
 );
